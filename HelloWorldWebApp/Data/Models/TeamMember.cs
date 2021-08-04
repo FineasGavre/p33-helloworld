afe6051f7f1a2a5c44a24d3ca4b3276992c2b28e
Fineas Gavre <fineasgavre@gmail.com>
2021-08-04 12:00:05 +0300

    Created TeamMember entity model.

---
+++ HelloWorldWebApp/Data/Models/TeamMember.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="TeamMember.cs" company="PRINCIPAL33">
+// Copyright (c) PRINCIPAL33. All rights reserved.
+// </copyright>
+
+namespace HelloWorldWebApp.Data.Models
+{
+    /// <summary>
+    /// Model for the TeamMember entity.
+    /// </summary>
+    public class TeamMember
+    {
+        /// <summary>
+        /// Entity Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// TeamMember Name
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
