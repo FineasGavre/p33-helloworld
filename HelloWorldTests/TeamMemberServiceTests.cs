ca078377f2988d52e541b4efd3a53b7ba0853c37
Fineas Gavre <fineasgavre@gmail.com>
2021-08-06 12:13:15 +0300

    Added TeamMemberService tests.

--- HelloWorldTests/TeamMemberServiceTests.cs
+++ HelloWorldTests/TeamMemberServiceTests.cs
@@ -1,4 +1,8 @@
 using HelloWorldWebApp;
+using HelloWorldWebApp.Data;
+using HelloWorldWebApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System;
 using Xunit;
 
@@ -6,10 +10,119 @@ namespace HelloWorldTests
 {
     public class TeamMemberServiceTests
     {
-        [Fact]
-        public void Test1()
+        public TeamMemberServiceTests()
         {
+            ContextOptions = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseSqlite("Filename=test.db")
+                .Options;
 
+            Seed();
+        }
+
+        public DbContextOptions<ApplicationContext> ContextOptions { get; }
+
+        private void Seed()
+        {
+            using (var context = new ApplicationContext(ContextOptions))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                var one = new TeamMember
+                {
+                    Name = "TeamMember1"
+                };
+
+                var two = new TeamMember
+                {
+                    Name = "TeamMember2"
+                };
+
+                var three = new TeamMember
+                {
+                    Name = "TeamMember3"
+                };
+
+                var four = new TeamMember
+                {
+                    Name = "TeamMember4"
+                };
+
+                context.AddRange(one, two, three, four);
+                context.SaveChanges();
+            }
+        }
+
+        [Fact]
+        public void CheckTeamMemberCount()
+        {
+            using var context = new ApplicationContext(ContextOptions);
+
+            // Assume
+            var teamService = new TeamMemberService(context);
+
+            // Act
+            var teamMembers = teamService.GetTeamMembers();
+
+            // Assert
+            Assert.Equal(4, teamMembers.Count);
+        }
+
+        [Fact]
+        public void DeleteTeamMember()
+        {
+            using var context = new ApplicationContext(ContextOptions);
+
+            // Assume
+            var teamService = new TeamMemberService(context);
+            var teamMember = teamService.GetTeamMembers()[0];
+
+            // Act
+            teamService.DeleteTeamMember(teamMember.Id);
+
+            // Assert
+            Assert.Equal(3, teamService.GetTeamMembers().Count);
+            Assert.DoesNotContain(teamMember, teamService.GetTeamMembers());
+        }
+
+        [Fact]
+        public void AddTeamMember()
+        {
+            using var context = new ApplicationContext(ContextOptions);
+
+            // Assume
+            var teamService = new TeamMemberService(context);
+            var teamMember = new TeamMember
+            {
+                Name = "TestMember"
+            };
+
+            // Act
+            teamService.AddTeamMember(teamMember);
+
+            // Assert
+            Assert.Equal(5, teamService.GetTeamMembers().Count);
+            Assert.Contains(teamMember, teamService.GetTeamMembers());
+        }
+
+        [Fact]
+        public void UpdateTeamMember()
+        {
+            using var context = new ApplicationContext(ContextOptions);
+
+            // Assume
+            var teamService = new TeamMemberService(context);
+            var teamMember = teamService.GetTeamMembers()[0];
+
+
+            // Act
+            teamMember.Name = "NewName";
+            teamService.UpdateTeamMember(teamMember);
+
+            // Assert
+            var updatedTeamMember = teamService.GetTeamMembers().ToList().Find(member => member.Id == teamMember.Id);
+
+            Assert.Equal("NewName", updatedTeamMember.Name);
         }
     }
 }
