62136c33e64f8dba0c5e50b59e01f25d02a0acce
Fineas Gavre <fineasgavre@gmail.com>
2021-08-06 09:56:55 +0300

    StyleCopped HelloWorldConsole.

---
+++ HelloWorldConsole/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+﻿// <copyright file="GlobalSuppressions.cs" company="PRINCIPAL33">
+// Copyright (c) PRINCIPAL33. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1200:Using directives should be placed correctly", Justification = "TeamDecision")]
--- HelloWorldConsole/Program.cs
+++ HelloWorldConsole/Program.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿// <copyright file="Program.cs" company="PRINCIPAL33">
+// Copyright (c) PRINCIPAL33. All rights reserved.
+// </copyright>
+
+using System;
 
 namespace HelloWorld
 {
-    class Program
+    /// <summary>
+    /// Main class for HelloWorldConsole.
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Main method for Program.
+        /// </summary>
+        /// <param name="args">Startup arguments.</param>
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
