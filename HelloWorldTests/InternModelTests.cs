b658ce0cc759a8782258b55b98a46a6d4318231e
Fineas Gavre <fineasgavre@gmail.com>
2021-08-11 12:17:15 +0300

    Added InternModelTest.

---
+++ HelloWorldTests/InternModelTests.cs
@@ -0,0 +1,33 @@
+﻿using HelloWorldWebApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HelloWorldTests
+{
+    public class InternModelTests
+    {
+        public InternModelTests()
+        {
+        }
+
+        [Fact]
+        public void CheckIfAgeIsCorrect()
+        {
+            // Assume
+            var intern = new Intern
+            {
+                Birthdate = new DateTime(2000, 8, 8)
+            };
+
+            // Act
+            var age = intern.GetAge();
+
+            // Assert
+            Assert.Equal(21, age);
+        }
+    }
+}
