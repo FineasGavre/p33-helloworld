22050878aefd2d5369ca353cf27906db20465596
Fineas Gavre <fineasgavre@gmail.com>
2021-08-11 12:12:48 +0300

    Added Age to Interns/Index

--- HelloWorldWebApp/Data/Models/LibraryResource.cs
+++ HelloWorldWebApp/Data/Models/LibraryResource.cs
@@ -10,7 +10,7 @@ namespace HelloWorldWebApp.Data.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Recommendation { get; set; }
-        public string URL {  get; set; }
+        public string URL { get; set; }
 
         public Skill Skill { get; set; }
         public int SkillId { get; set; }
