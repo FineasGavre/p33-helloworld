cac0bb08667d8496ddf25789d638ae44d3d2e976
Fineas Gavre <fineasgavre@gmail.com>
2021-08-13 12:56:55 +0300

    Added dependencies.

--- HelloWorldWebApp/Startup.cs
+++ HelloWorldWebApp/Startup.cs
@@ -11,6 +11,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace HelloWorldWebApp
 {
@@ -45,6 +49,16 @@ namespace HelloWorldWebApp
 
             services.AddScoped<ITeamMemberService, TeamMemberService>();
             services.AddScoped<IWeatherService, WeatherService>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hello World API", Version = "v1" });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         /// <summary>
@@ -54,11 +68,18 @@ namespace HelloWorldWebApp
         /// <param name="env">WebHostEnvironment object.</param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseMigrationsEndPoint();
                 app.UseBrowserLink();
+
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "HelloWorldApi");
+                });
             }
             else
             {
